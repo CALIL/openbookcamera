583743c122c2ac8ed91342ffaf3578abc040d8d7
Satoshi Deguchi <deguchik@gmail.com>
2020-04-02 15:05:04 +0900

    update

---
+++ web/src/js/main.js
@@ -0,0 +1,9 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+var iceCream = 'チョコレート';
+if (iceCream === 'チョコレート') {
+  alert('やった、チョコレートアイス大好き！');    
+} else {
+  alert('あれれ、でもチョコレートは私のお気に入り......');    
+}
